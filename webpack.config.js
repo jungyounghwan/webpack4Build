f88c4eff4ea0416f1b60257d861e1023058065ba
jung <jung@saramin.co.kr>
2019-07-16 16:37:26 +0900

    npm folder bulid modify

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,55 @@
 const webpack = require('webpack');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = (env, options) => {
-    const config = {
-        entry: {
-            app: ['@babel/polyfill', './npm-name/app.js']
-        },
-        output: {
-            filename: '[name].js',
-            path: path.resolve(__dirname, './npm-name/dist/'),
-            libraryTarget: 'var',
-            library: 'UI',
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    include: path.join(__dirname, './npm-name/app.js'),
-                    use: {
-                        loader : 'babel-loader',
-                        options : {
-                            presets : ['@babel/preset-env']
-                        }
+module.exports = {
+    entry: {
+        app: ['@babel/polyfill', './motion/app.js']
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './motion/dist/'),
+        libraryTarget: 'var',
+        library: 'UI'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: { sourceMap: true },
+                    },
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: {
+                    loader : 'babel-loader',
+                    options : {
+                        presets : ['@babel/preset-env']
                     }
-                },
-                {
-                    test: /\.(s*)css$/, // match any .scss or .css file,
-                    use: [
-                        "style-loader",
-                        "css-loader",
-                        "sass-loader"
-                    ]
                 }
-            ]
-        },
-        plugins: [
-            new webpack.HotModuleReplacementPlugin(),
+            }
+        ]
+    },
+    optimization : {
+        minimizer: [
             new TerserPlugin()
-        ],
-        devtool: 'source-map'
-    }
-
-    if(options.mode === 'development') {
-
-        config.devServer = {
-            hot: true,
-            contentBase: path.join(__dirname, '/npm-name/'),
-            port: 7777
-        };
-
-    } else {
-        // Production ¼³Á¤
-        config.plugins = [
-            new CleanWebpackPlugin(['dist'])
-        ];
-
-        config.optimization = {
-            minimizer: [
-                new TerserPlugin({
-                    cache: true,
-                    sourceMap: true,
-                }),
-            ],
-        };
-    }
-
-    return config;
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin('dist'),
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+    devtool: 'source-map'
 };
